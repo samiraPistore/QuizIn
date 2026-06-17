3bfa07fe492262c5cecaec58e590f9f6772df829
Samira Pistore <sami.pistore@gmail.com>
2026-06-17 13:44:54 -0300

    Primeiro commit: definição das roas e cores principais

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_in/routes/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: const Color(0xFF003764),
+          primary: Color(0xFF003764),
+          secondary: Color(0xFF0084AD),
+          tertiary: Color(0xFF333333),
+          
+        ),
+         
+      ),
+      initialRoute: AppRoutes.login,
+      routes: {
+        AppRoutes.login: (ctx) => LoginPage(),
+        AppRoutes.recSenha: (ctx) => RecSenhaPage(),
+        AppRoutes.home: (ctx) => HomePage(),
+        AppRoutes.respostas: (ctx) => RespostasPage(),
+      },
+    );
+  }
+}
---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,7 @@
+class AppRoutes {
+  static const login = '/';
+  static const recSenha = '/rec_senha';
+  static const home = '/home';
+  static const respostas = 'resps';
+  
+}
